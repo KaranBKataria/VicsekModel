4c8ce405cc85ec7e1d274ea7e10c01044734d853
Karan Kataria <103467529+KaranBKataria@users.noreply.github.com>
2026-07-14 00:04:18 +0100

    Replaced const array reference with std::span

--- include/math_utils.h
+++ include/math_utils.h
@@ -1,9 +1,9 @@
 #ifndef MATH_UTILS_HPP
 #define MATH_UTILS_HPP
 
-#include <array>
 #include <cstdint>
+#include <span>
 
-double minkowski_distance(std::int16_t p, const std::array<double,2>& v1, const std::array<double,2>& v2);
+double minkowski_distance(std::int16_t p, std::span<double> v1, std::span<double> v2);
 
 #endif
